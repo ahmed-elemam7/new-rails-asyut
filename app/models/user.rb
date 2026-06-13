ef3d32281b0a41cf7c94a207bb1d7a22961701dc
ahmed-elemam7 <ahmed.elemam.elazouni@gmail.com>
2026-06-13 12:54:16 +0300

    Add user management functionality with CRUD operations and validations

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :name, :dob, :email, :phone_number, :address, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+end
---
+++ db/migrate/20260613100100_add_address_to_users.rb
@@ -0,0 +1,6 @@
+class AddAddressToUsers < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :address, :string, null: false, default: ""
+    change_column_default :users, :address, nil
+  end
+end
