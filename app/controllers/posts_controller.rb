802a48d23bed81149cc1d6aecaa29636e2928581
ahmed-elemam7 <ahmed.elemam.elazouni@gmail.com>
2026-06-13 12:45:54 +0300

    Implement CRUD actions for posts and add routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,57 @@
 class PostsController < ApplicationController
-  # your code goes here
+  before_action :set_post, only: %i[show edit update destroy]
+
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # PATCH/PUT /posts/:id
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: "Post was successfully destroyed."
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
