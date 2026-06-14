7125f07b6c0e89779427fd792dad1d1954ca2b4c
ahmed-elemam7 <ahmed.elemam.elazouni@gmail.com>
2026-06-14 14:42:46 +0300

    Refactor associations in Post and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :created_posts, class_name: "Post", foreign_key: "creator_id"
-    has_many :post_editors, foreign_key: :editor_id
+    has_many :posts, dependent: :destroy
+    has_many :created_posts, class_name: "Post", foreign_key: "creator_id", dependent: :destroy
+    has_many :post_editors, foreign_key: :editor_id, dependent: :destroy
     has_many :edited_posts, through: :post_editors, source: :post
 end
--- db/migrate/20260614105900_create_editors_posts_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateEditorsPostsJoinTable < ActiveRecord::Migration[8.1]
-  def change
-    create_table :editors_posts, id: false do |t|
-      t.references :editor, null: false, foreign_key: { to_table: :users }
-      t.references :post, null: false, foreign_key: true
-    end
-  end
-end
